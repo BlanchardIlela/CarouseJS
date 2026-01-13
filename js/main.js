4b9b9cf48ce2e1fa6d84caee51c2903ad475868d
BlanchardIlela <blancilela@gmail.com>
2026-01-13 05:37:20 +0100

    Création de nouvelles carousel

---
+++ js/main.js
@@ -0,0 +1,44 @@
+class Carousel {
+
+    /**
+     * @param {HTMLElement} element 
+     * @param {Object} options.slidesToScroll Nombre d'éléments à faire défiler
+     * @param {Object} options.slidesVisible Nombre d'éléments visible dans un slide  
+     */
+    constructor (element, options = {}) {
+        this.element = element
+        this.options = Object.assign({}, {
+            slidesToScroll: 1,
+            slidesVisible: 1
+        }, options)
+        this.children = [].slice.call(element.children)
+        let root = this.createDivWithClass('carousel')
+        let container = this.createDivWithClass('carousel__container')
+        root.appendChild(container)
+        this.element.appendChild(root)
+        this.children.forEach((child) => {
+            let item = this.createDivWithClass('carousel__item')
+            item.appendChild(child)
+            container.appendChild(item)
+        })
+    }
+
+    /**
+     * 
+     * @param {string} className 
+     * @returns {HTMLElement}
+     */
+    createDivWithClass (className) {
+        let div = document.createElement('div')
+        div.setAttribute('class', className)
+        return div
+    }
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Carousel(document.querySelector('#carousel1'), {
+    slidesVisible: 3
+    })
+})
